06892c5662694a3bea554d45d038d34eae6dc63a
aidris823 <anandidris@Anands-MacBook-Pro.local>
2018-09-17 04:21:18 -0400

    Not working oml I regret not knowing how to use pointers

---
+++ pointer_lab.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+  unsigned int j_bezos_networth = 1616000000;
+  char *swiss_bank = &j_bezos_networth; /* A tax free haven to hold the money*/
+  printf("In hex: %x \n",j_bezos_networth); //In hex form
+  //int i;
+
+  //Each byte printed
+  for (int i = 0; i < sizeof(int); i++){
+    printf("Byte no. %d : %hhx \n",i,*swiss_bank);
+    swiss_bank++;
+  }
+
+  swiss_bank = &j_bezos_networth;
+  //By 1 increment
+  for(int i = 0; i < sizeof(int); i++){
+    *swiss_bank++;
+    swiss_bank++;
+    printf("Inc by 1; Decimal: %d Hexidecimal: %x \n",j_bezos_integer,j_bezos_integer);
+  }
+  
+  swiss_bank = &j_bezos_networth;
+  //By 16 increment
+  for (int i = 0; i < sizeof(int); i++){
+    *swiss_bank += 16;
+    swiss_bank++;
+    printf("Inc by 16; Decimal %d Hexidecimal: %x \n",j_bezos_integer,j_bezos_integer);
+  }
+
+  return 0; //YOU WIN!
+}
