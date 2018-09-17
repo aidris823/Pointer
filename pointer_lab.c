992743d24769272c2082eb0d8a1891514e5d8215
aidris823 <anandidris@Anands-MacBook-Pro.local>
2018-09-17 04:36:42 -0400

    Okay hex form and each byte works, which is miraculous enpugh.  I'd like to do 100% of this, however.

--- pointer_lab.c
+++ pointer_lab.c
@@ -5,21 +5,21 @@ int main(){
   unsigned int j_bezos_networth = 1616000000;
   char *swiss_bank = &j_bezos_networth; /* A tax free haven to hold the money*/
   printf("In hex: %x \n",j_bezos_networth); //In hex form
-  //int i;
-
+  
+  
   //Each byte printed
   for (int i = 0; i < sizeof(int); i++){
     printf("Byte no. %d : %hhx \n",i,*swiss_bank);
     swiss_bank++;
   }
-
-  swiss_bank = &j_bezos_networth;
-  //By 1 increment
+  /*
+  swiss_bank = &j_bezos_networth;   //By 1 increment
   for(int i = 0; i < sizeof(int); i++){
     *swiss_bank++;
     swiss_bank++;
     printf("Inc by 1; Decimal: %d Hexidecimal: %x \n",j_bezos_integer,j_bezos_integer);
   }
+  print
   
   swiss_bank = &j_bezos_networth;
   //By 16 increment
@@ -28,6 +28,6 @@ int main(){
     swiss_bank++;
     printf("Inc by 16; Decimal %d Hexidecimal: %x \n",j_bezos_integer,j_bezos_integer);
   }
-
+  */
   return 0; //YOU WIN!
 }
