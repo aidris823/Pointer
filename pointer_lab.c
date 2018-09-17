3d60bee246abfaf8ea5f674d7d0d1c5d020d2ac3
aidris823 <anandidris@Anands-MacBook-Pro.local>
2018-09-17 04:47:43 -0400

    HAH GOT YOU NOW

--- pointer_lab.c
+++ pointer_lab.c
@@ -12,22 +12,39 @@ int main(){
     printf("Byte no. %d : %hhx \n",i,*swiss_bank);
     swiss_bank++;
   }
-  /*
+  
   swiss_bank = &j_bezos_networth;   //By 1 increment
   for(int i = 0; i < sizeof(int); i++){
     *swiss_bank++;
     swiss_bank++;
-    printf("Inc by 1; Decimal: %d Hexidecimal: %x \n",j_bezos_integer,j_bezos_integer);
+    printf("Inc by 1; In decimal: %d \n In Hexidecimal: %x \n",j_bezos_networth,j_bezos_networth);
   }
-  print
-  
+     
   swiss_bank = &j_bezos_networth;
   //By 16 increment
   for (int i = 0; i < sizeof(int); i++){
     *swiss_bank += 16;
     swiss_bank++;
-    printf("Inc by 16; Decimal %d Hexidecimal: %x \n",j_bezos_integer,j_bezos_integer);
+    printf("Inc by 16; Decimal: %d \n Hexidecimal: %x \n",j_bezos_networth,j_bezos_networth);
   }
-  */
-  return 0; //YOU WIN!
+  
+  return 0; //YOU WON!
+
+  /* Anand got 1 exp.
+
+The assignment left a present!
+
+Inside the present, there was one-two hours of angry coding!
+
+Anand can't waste any more time.
+
+Manage your time better?
+
+Yes No<
+
+Anand will continue to procrastinate these assignments. 
+
+=(
+
+  */ 
 }
